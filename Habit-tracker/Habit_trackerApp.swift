3e1fdec6e1a345f221035da6de99ffbeaac228f4
GreedyGobling <greedygobling@gmail.com>
2026-04-27 13:27:23 +0200

    Initial Commit

---
+++ Habit-tracker/Habit_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_trackerApp.swift
+//  Habit-tracker
+//
+//  Created by robin on 2026-04-27.
+//
+
+import SwiftUI
+
+@main
+struct Habit_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
