2c4706883861b37180cf1ca1a16c46acd15e394b
GreedyGobling <greedygobling@gmail.com>
2026-05-05 14:33:48 +0200

    habit streaks

--- Habit-tracker/Models/Habit.swift
+++ Habit-tracker/Models/Habit.swift
@@ -5,6 +5,8 @@ class Habit {
     var title: String
     var isDone: Bool = false
     var completed: Int = 0
+    var currentStreak: Int = 0
+    var maxStreak: Int = 0
 
     init(title: String) {
         self.title = title
